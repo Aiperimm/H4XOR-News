522dd34a58c64bf3746775479089d8613744fbab
AIPERIM AKISHEVA <AIPERIM1995@GMAIL.COM>
2022-03-14 20:57:17 -0400

    Initial Commit

---
+++ H4XOR News/H4XOR_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4XOR_NewsApp.swift
+//  H4XOR News
+//
+//  Created by Aiperim Akisheva on 3/14/22.
+//
+
+import SwiftUI
+
+@main
+struct H4XOR_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
